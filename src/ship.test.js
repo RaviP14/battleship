67cb57197f64ed0055fdd4011e83527d9a7a6f41
RaviP14 <ravi.patel14@outlook.com>
2021-11-06 16:10:45 +0000

    Added tests for ship factory.

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,5 +1,21 @@
 import makeShip from './ship';
 
-test('test1', () => {
-  expect(makeShip.x()).toBe();
+test('length of ship', () => {
+  const ship1 = makeShip.newShip(2);
+
+  expect(ship1.length()).toBe(2);
+});
+
+test('is the ship hit', () => {
+  const ship2 = makeShip.newShip(3);
+
+  expect(ship2.hit()).toBe('hit');
+});
+
+test('is the ship sunk', () => {
+  const ship3 = makeShip.newShip(3);
+  ship3.hit();
+  ship3.hit();
+  ship3.hit();
+  expect(ship3.isSunk).toBe('sunk');
 });
