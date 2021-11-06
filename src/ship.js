186612bf34f0bfac12da7a398c298ae2c45e705a
RaviP14 <ravi.patel14@outlook.com>
2021-11-06 19:53:03 +0000

    Edited tests and added code for ship factory.

--- src/ship.js
+++ src/ship.js
@@ -1,3 +1,41 @@
-const makeShip = (() => {})();
+const makeShip = (() => {
+  const ship = (length) => {
+    const getLength = () => length;
+
+    const position = Array.from(Array(length).keys());
+
+    const hit = (n) => {
+      if (position[n] !== 'hit') {
+        position[n] = 'hit';
+      }
+      return position[n];
+    };
+
+    const isEqual = (array) => {
+      return array.every((value) => value === 'hit');
+    };
+
+    const isSunk = () => {
+      const equal = isEqual(position);
+      if (equal) {
+        const sunk = 'sunk';
+        return sunk;
+      }
+      const notSunk = 'not sunk';
+      return notSunk;
+    };
+
+    return { getLength, hit, isSunk };
+  };
+
+  function newShip(length) {
+    const ship1 = ship(length);
+    return ship1;
+  }
+
+  return {
+    newShip,
+  };
+})();
 
 export default makeShip;
--- src/ship.test.js
+++ src/ship.test.js
@@ -3,19 +3,19 @@ import makeShip from './ship';
 test('length of ship', () => {
   const ship1 = makeShip.newShip(2);
 
-  expect(ship1.length()).toBe(2);
+  expect(ship1.getLength()).toBe(2);
 });
 
 test('is the ship hit', () => {
   const ship2 = makeShip.newShip(3);
 
-  expect(ship2.hit()).toBe('hit');
+  expect(ship2.hit(2)).toBe('hit');
 });
 
 test('is the ship sunk', () => {
   const ship3 = makeShip.newShip(3);
-  ship3.hit();
-  ship3.hit();
-  ship3.hit();
-  expect(ship3.isSunk).toBe('sunk');
+  ship3.hit(0);
+  ship3.hit(1);
+  ship3.hit(2);
+  expect(ship3.isSunk()).toBe('sunk');
 });
