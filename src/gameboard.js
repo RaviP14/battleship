7eb8a19fe0633672ff32d6a1e3197b5d045abd84
RaviP14 <ravi.patel14@outlook.com>
2021-11-09 16:43:02 +0000

    gameboard factory adds ship to gameboard and test passes.

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,3 +1,51 @@
-const makeGameboard = (() => {})();
+import makeShip from './ship';
+
+const makeGameboard = (() => {
+  const gameboard = () => {
+    const board = [
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    ];
+    const ships = [];
+
+    const placeship = (x, y, position, length, name) => {
+      const ship = makeShip.newShip(length, name);
+      ships.push(ship);
+      let coordX = x;
+      let coordY = y;
+      if (!Number.isNaN(board[coordY][coordX])) {
+        for (let i = 0; i < length; i += 1) {
+          board[coordY][coordX] = ship.getName();
+          if (position === 'vertical') {
+            coordY += 1;
+          } else if (position === 'horizontal') {
+            coordX += 1;
+          }
+        }
+      }
+      return `ship: (${board[y][x]}) placed at (${x}, ${y})`;
+    };
+
+    return { placeship };
+  };
+
+  function newGameboard() {
+    const gameboard1 = gameboard();
+
+    return gameboard1;
+  }
+
+  return {
+    newGameboard,
+  };
+})();
 
 export default makeGameboard;
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -2,9 +2,12 @@ import makeGameboard from './gameboard';
 
 test('place ship on gameboard', () => {
   const x = 2;
-  const y = 'b';
+  const y = 1;
   const size = 2;
-  expect(makeGameboard.placeShip(x, y, size)).toBe(
-    `ship placed at (${x}, ${y})`
+  const position = 'vertical';
+  const name = 'Battleship';
+  const board1 = makeGameboard.newGameboard();
+  expect(board1.placeship(x, y, position, size, name)).toBe(
+    'ship: (Battleship) placed at (2, 1)'
   );
 });
