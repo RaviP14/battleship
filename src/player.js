67364043884b23efb949e6fccf3810b331bfffe5
RaviP14 <ravi.patel14@outlook.com>
2021-11-11 20:09:39 +0000

    Added code for player attack and updated test.

--- src/player.js
+++ src/player.js
@@ -1,3 +1,23 @@
-const makePlayer = (() => {})();
+import makeGameboard from './gameboard';
+
+const makePlayer = (() => {
+  const player = (name) => {
+    const getName = () => name;
+    const playersGamebaoard = makeGameboard.newGameboard();
+    const attack = (target, x, y) => {
+      return target.playersGamebaoard.receiveAttack(x, y);
+    };
+    return { getName, playersGamebaoard, attack };
+  };
+
+  function newPlayer(name) {
+    const player0 = player(name);
+    return player0;
+  }
+
+  return {
+    newPlayer,
+  };
+})();
 
 export default makePlayer;
--- src/player.test.js
+++ src/player.test.js
@@ -4,11 +4,13 @@ test('attack the enemy', () => {
   const player1 = makePlayer.newPlayer();
   const player2 = makePlayer.newPlayer();
 
-  player1.gameboard.placeship(1, 2, 'vertical', 3, 'Enforcer');
-  player2.gameboard.placeship(5, 4, 'horizontal', 3, 'Enforcer');
+  player1.playersGamebaoard.placeship(1, 2, 'vertical', 3, 'Enforcer');
+  player2.playersGamebaoard.placeship(5, 4, 'horizontal', 3, 'Enforcer');
+  player2.playersGamebaoard.placeship(3, 3, 'horizontal', 2, 'Marina');
 
-  expect(player1.attack(2, 2)).tobe('miss');
-  expect(player2.attack(4, 2)).tobe('miss');
-  expect(player1.attack(1, 3)).tobe('hit');
-  expect(player2.attack(6, 4)).tobe('hit');
+  expect(player1.attack(player2, 2, 2)).toBe('miss');
+  expect(player2.attack(player1, 4, 2)).toBe('miss');
+  expect(player1.attack(player2, 6, 4)).toBe('hit');
+  expect(player2.attack(player1, 1, 3)).toBe('hit');
+  expect(player1.attack(player2, 3, 3)).toBe('hit');
 });
