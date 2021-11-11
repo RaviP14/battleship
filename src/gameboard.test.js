4c5fb4679b959ae7cfdb93d5965d0ce9abc35a03
RaviP14 <ravi.patel14@outlook.com>
2021-11-11 13:49:06 +0000

    Added tests to check if all ships are sunk.

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -29,3 +29,24 @@ test('Missed attacks', () => {
   expect(board1.getMissedAttack(4, 2)).toStrictEqual([2, 4]);
   expect(board1.getMissedAttack(1, 1)).toBe('not a missed attack');
 });
+
+test('all sunk', () => {
+  const board1 = makeGameboard.newGameboard();
+  board1.placeship(1, 2, 'vertical', 2, 'Cargo');
+  board1.placeship(3, 3, 'horizontal', 1, 'Miniship');
+  board1.receiveAttack(1, 2);
+  board1.receiveAttack(1, 3);
+  board1.receiveAttack(3, 3);
+
+  expect(board1.allSunk()).toBe('all sunk');
+});
+
+test('not all sunk', () => {
+  const board1 = makeGameboard.newGameboard();
+  board1.placeship(1, 2, 'vertical', 2, 'Cargo');
+  board1.placeship(3, 3, 'horizontal', 1, 'Miniship');
+  board1.receiveAttack(1, 2);
+  board1.receiveAttack(3, 3);
+
+  expect(board1.allSunk()).toBe('not all sunk');
+});
