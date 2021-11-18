c6c03c730a1b254a225479101418ae007547080b
RaviP14 <ravi.patel14@outlook.com>
2021-11-18 16:50:53 +0000

    Added function that converts index value to coordinates and updates its test.

--- src/interact.js
+++ src/interact.js
@@ -1,32 +1,22 @@
 const interact = (() => {
-  const playGame = document.getElementById('playGame');
-  const playGameBtn = playGame;
+  function convertToCoords(index, gridColLength) {
+    const rowPosition = Math.floor(index / gridColLength);
+    const colPosition = index % gridColLength;
+    const coords = {
+      y: rowPosition,
+      x: colPosition,
+    };
 
-  const gridP = document.getElementById('gridDivP');
-  // gridP.style.display = 'none';
-  const gridC = document.getElementById('gridDivC');
-  // gridC.style.display = 'none';
-
-  function makeGrid(gridName, container, rows, columns) {
-    const grid = document.getElementById(gridName);
-    grid.style.setProperty('--grid-rows', rows);
-    grid.style.setProperty('--grid-cols', columns);
-    for (let i = 0; i < rows * columns; i += 1) {
-      const gridDiv = document.createElement('div');
-      gridDiv.className = 'gridSqaure';
-      gridDiv.style.height = `${400 / rows}px`;
-      gridDiv.style.width = `${400 / columns}px`; // change to rem?
-      grid.appendChild(gridDiv);
-    }
-    container.appendChild(grid);
+    return coords;
   }
 
-  // const cGrid = document.getElementById('computersGrid');
+  function getCoords(index, gridColLength) {
+    const coords = convertToCoords(index, gridColLength);
+    return coords;
+  }
 
-  makeGrid('playersGrid', gridP, 10, 10);
-  makeGrid('computersGrid', gridC, 10, 10);
   return {
-    playGameBtn,
+    getCoords,
   };
 })();
 
--- src/interact.test.js
+++ src/interact.test.js
@@ -1,7 +1,11 @@
 import interact from './interact';
 
 test('get coordinates from dom', () => {
-  const getCoord = interact.getCoords(7);
+  const getCoord = interact.getCoords(7, 10);
+  const getCoord2 = interact.getCoords(89, 10);
+  const getCoord3 = interact.getCoords(34, 10);
 
-  expect(getCoord).toBe({ x: 7, y: 0 });
+  expect(getCoord).toMatchObject({ x: 7, y: 0 });
+  expect(getCoord2).toMatchObject({ x: 9, y: 8 });
+  expect(getCoord3).toMatchObject({ x: 4, y: 3 });
 });
