d152e7439994d992a40f7a39ae861111565dd452
RaviP14 <ravi.patel14@outlook.com>
2021-11-19 19:47:59 +0000

    Added test to convert coordinates from computer AI to index value.

--- src/interact.test.js
+++ src/interact.test.js
@@ -9,3 +9,39 @@ test('get coordinates from dom', () => {
   expect(getCoord2).toMatchObject({ x: 9, y: 8 });
   expect(getCoord3).toMatchObject({ x: 4, y: 3 });
 });
+
+test('get index from coords 1 hit', () => {
+  // Get values as [y, x] from
+  const getIndex = interact.getIndex({ yVal: 7, xVal: 3 }, 10);
+  const getIndex2 = interact.getIndex({ yVal: 4, xVal: 9 }, 10);
+
+  expect(getIndex).toMatchObject({ index1: 73 });
+  expect(getIndex2).toMatchObject({ index1: 49 });
+});
+
+test('get index from coords 2 hits', () => {
+  const getIndex3 = interact.getIndex(
+    [
+      { xVal: 6, yVal: 6, attacked: 'hit' },
+      { xVal: 3, yVal: 4, attacked: 'missed' },
+    ],
+    10
+  );
+
+  expect(getIndex3).toMatchObject({ index1: 66, index2: 43 });
+});
+
+test('get index from coords 3 hits', () => {
+  // Get values as [y, x] from
+  const getIndex4 = interact.getIndex(
+    [
+      [
+        { xVal: 6, yVal: 6, attacked: 'hit' },
+        { xVal: 3, yVal: 4, attacked: 'missed' },
+      ],
+      { xVal: 2, yVal: 2, attacked: 'missed' },
+    ],
+    10
+  );
+  expect(getIndex4).toMatchObject({ index1: 66, index2: 43, index3: 22 });
+});
