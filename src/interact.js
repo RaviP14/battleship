8d461c32e4a300d179cacc9de4a9343416d1708f
RaviP14 <ravi.patel14@outlook.com>
2021-11-20 16:26:16 +0000

    Updated code to add attack status to return object.

--- src/interact.js
+++ src/interact.js
@@ -29,36 +29,49 @@ const interact = (() => {
     if (isArr === '[object Object]') {
       const y = array.yVal;
       const x = array.xVal;
+      const attack = array.attacked;
 
       const val1 = y * colLength;
       const index = val1 + x;
-      finalVal = { index1: index };
+      finalVal = { index1: index, attacked: attack };
     } else if (isArr === '[object Array]' && isObject === true) {
       const y1 = array[0].yVal;
       const x1 = array[0].xVal;
+      const attack1 = array[0].attacked;
 
       const y2 = array[1].yVal;
       const x2 = array[1].xVal;
+      const attack2 = array[1].attacked;
 
       const indexs1 = y1 * colLength + x1;
       const indexs2 = y2 * colLength + x2;
 
-      finalVal = { index1: indexs1, index2: indexs2 };
+      finalVal = {
+        attack1: { index1: indexs1, attacked: attack1 },
+        attack2: { index2: indexs2, attacked: attack2 },
+      };
     } else if (isArr === '[object Array]' && isObject === false) {
       const y1 = array[0][0].yVal;
       const x1 = array[0][0].xVal;
+      const attack1 = array[0][0].attacked;
 
       const y2 = array[0][1].yVal;
       const x2 = array[0][1].xVal;
+      const attack2 = array[0][1].attacked;
 
       const y3 = array[1].yVal;
       const x3 = array[1].xVal;
+      const attack3 = array[1].attacked;
 
       const indexs1 = y1 * colLength + x1;
       const indexs2 = y2 * colLength + x2;
       const indexs3 = y3 * colLength + x3;
 
-      finalVal = { index1: indexs1, index2: indexs2, index3: indexs3 };
+      finalVal = {
+        attack1: { index1: indexs1, attacked: attack1 },
+        attack2: { index2: indexs2, attacked: attack2 },
+        attack3: { index3: indexs3, attacked: attack3 },
+      };
     }
     return finalVal;
   }
@@ -67,7 +80,7 @@ const interact = (() => {
     const val = convertToIndex(array, colLength);
     return val;
   }
-
+  // render hit or miss on page function?
   return {
     getCoords,
     getIndex,
