4ff0599f01fea6d60505e66763d6627cb8ec978c
RaviP14 <ravi.patel14@outlook.com>
2021-11-21 17:03:53 +0000

    Player now renders attacks on computers gameboard.

--- src/index.js
+++ src/index.js
@@ -12,19 +12,22 @@ import elem from './elem';
     player1.playersGamebaoard.placeship(1, 1, 'horizontal', 3, 'submarine');
 
     computer.playersGamebaoard.placeship(1, 1, 'vertical', 2, 'trooper');
-    computer.playersGamebaoard.placeship(8, 5, 'horizontal', 4, 'attack ship');
+    computer.playersGamebaoard.placeship(6, 5, 'horizontal', 4, 'attack ship');
     computer.playersGamebaoard.placeship(0, 3, 'vertical', 3, 'submarine');
-  });
 
-  document.addEventListener('click', (e) => {
-    if (e.target.matches('.gridSquare')) {
-      const child = e.target;
-      const parent = child.parentNode;
-      const index = Array.prototype.indexOf.call(parent.children, child);
-      if (parent.id === 'computersGrid') {
-        const coord = interact.getCoords(index, 10);
-        // use coord.x & coord.y in player attack
+    document.addEventListener('click', (e) => {
+      if (e.target.matches('.gridSquare')) {
+        const child = e.target;
+        const parent = child.parentNode;
+        const index = Array.prototype.indexOf.call(parent.children, child);
+        if (parent.id === 'computersGrid' && child.textContent === '') {
+          const coord = interact.getCoords(index, 10);
+          const xCoord = coord.x;
+          const yCoord = coord.y;
+          const attackResult = player1.attack(computer, xCoord, yCoord);
+          interact.renderAttackP(attackResult, child);
+        }
       }
-    }
+    });
   });
 })();
